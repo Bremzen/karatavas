82d474878c5dd5271ab71162d1cdc1eccfca0213
Aleksis <aleksisdz6@gmail.com>
2024-06-12 13:20:04 +0300

    added win and lose screen, replayability

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 '''
 =======Spēles mošķi========
 '''
-
+Chars = {"Cilvēks": "Cilvēks", "Spoks": "Spoks", "Nāve": "Nāve"}
 Cilvēks = [
     [["create_line"], [550, 100, 550, 420]],
     [["create_line"], [500, 100, 550, 150]],
@@ -31,8 +31,8 @@ Spoks = [
     [["create_line"], [380, 260, 420, 260]],
 ]
 Nāve = [
-    [["create_line"],[550, 100, 550, 420]], 
-    [["create_line"], [500, 100, 550, 150]], 
+    [["create_line"], [550, 100, 550, 420]],
+    [["create_line"], [500, 100, 550, 150]],
     [["create_line"], [400, 100, 550, 100]],
     [["create_line"], [400, 100, 400, 150]],
     [["create_oval"], [375, 150, 425, 200]],
@@ -46,35 +46,42 @@ Nāve = [
 '''
 =======Vārda iegūšana========
 '''
-with open("vārdi.txt","r", encoding="utf-8") as f:
+with open("vārdi.txt", "r", encoding="utf-8") as f:
     vārdi = [rinda.strip() for rinda in f]
 
 def jauns_vārds(vārdi):
-    return vārdi[randint(0,len(vārdi)-1)]
+    return vārdi[randint(0, len(vārdi) - 1)]
 
 '''
 =======Jauna spēle========
 '''
 
 def jauna_spēle():
-    global vārds, char, kļūdas, pieļaujamais_kļūdu_skaits, minētie_burti, vārds2
+    global vārds, char, kļūdas, pieļaujamais_kļūdu_skaits, minētie_burti, vārds2, Char
+    root.bind("<Key>", pogas_spiediens)
     vārds = jauns_vārds(vārdi)
     vārds2 = vārds
-    print(vārds)#priekš testēšanas
+    #print(vārds)  # priekš testēšanas
     char = ''
     kļūdas = 0
     pieļaujamais_kļūdu_skaits = 10
     minētie_burti = ''
     dzēst_minēšanas_vārda_gui()
-    Char.dzēst_parādītās_detaļas() #nodzēš iepriekšējo mošķi
-    #noņem sākuma ekrānu
+    Char.dzēst_parādītās_detaļas()  # nodzēš iepriekšējo mošķi
+    # noņem sākuma ekrānu
     ekrans.delete(nosaukums)
+    ekrans.delete(mošķa_teksts)
     poga.destroy()
+    mošķu_izvēle.destroy()
     galvenais_gui()
 
 def pirms_jauna_spēle():
-    ekrans.delete(zaudes_teksts)
-    pogaZaude.place_forget()
+    if 'zaudes_teksts' in globals():
+        ekrans.delete(zaudes_teksts)
+    if 'uzvaras_teksts' in globals():
+        ekrans.delete(uzvaras_teksts)
+    poga_spēlēt_atkal.place_forget()
+    dzēst_minēšanas_vārda_gui()
     dzēst_minēšanas_burta_gui()
     jauna_spēle()
 
@@ -86,27 +93,36 @@ root = Tk()
 root.title('Karātavas')
 ekrans = Canvas(root, width=600, height=500)
 ekrans.pack()
-x,y = 600,500
+x, y = 600, 500
+chosen_char = StringVar(root)
+chosen_char.set("Cilvēks")  # Default value
+
 def galvenais_gui():
-    global minējuma_burts, minējuma_teksts
-    minējuma_teksts = ekrans.create_text(10,200, text='Tavs minētais burts: ', font=("Times New Roman", 20), anchor="w")
-    minējuma_burts = ekrans.create_text(230,200, text=char, font=("Times New Roman", 20), anchor="w")
+    global minējuma_burts, minējuma_teksts, Char
+    selected = chosen_char.get()
+    if selected == "Cilvēks":
+        Char = KarātavasDaļas(Cilvēks)
+    elif selected == "Spoks":
+        Char = KarātavasDaļas(Spoks)
+    else:
+        Char = KarātavasDaļas(Nāve)
+    
+    minējuma_teksts = ekrans.create_text(30, 200, text='Tavs minētais burts: ', font=("Times New Roman", 20), anchor="w")
+    minējuma_burts = ekrans.create_text(250, 200, text=char, font=("Times New Roman", 20), anchor="w")
     minēšanas_vārda_gui()
 
 def minēšanas_vārda_gui():
     for i in range(len(vārds2)):
         var_name = f'varline_{i}'
-        globals()[var_name] = ekrans.create_line(30+i*40,250,50+i*40,250)
+        globals()[var_name] = ekrans.create_line(30 + i * 40, 250, 50 + i * 40, 250)
 
 def minēšanas_burta_gui():
     global vārds2
     while char in vārds2:
-         i = vārds2.index(char)
-         print(i)
-         var_name = f'varlet_{i}'
-         globals()[var_name] = ekrans.create_text(40 + i * 40, 240, text=char.upper(), font=("Times New Roman", 20))
-         vārds2 = vārds2[:i] + '0' + vārds2[i+1:]
-            
+        i = vārds2.index(char)
+        var_name = f'varlet_{i}'
+        globals()[var_name] = ekrans.create_text(40 + i * 40, 240, text=char.upper(), font=("Times New Roman", 20))
+        vārds2 = vārds2[:i] + '0' + vārds2[i + 1:]
 
 def dzēst_minēšanas_vārda_gui():
     for i in range(len(vārds2)):
@@ -122,45 +138,55 @@ def dzēst_minēšanas_burta_gui():
 
 def zaudēšanas_gui():
     global zaudes_teksts
-    ekrans.delete(minējuma_burts,minējuma_teksts)
+    ekrans.delete(minējuma_burts, minējuma_teksts)
     zaudes_teksts = ekrans.create_text(80, 150, anchor='w', text="Tu zaudēji!", fill="black", font=("Helvetica", 30))
-    pogaZaude.place(x=x/2,y=y/2, anchor="center", height='30',width='150')
+    poga_spēlēt_atkal.place(x=120, y=460, anchor="center", height='30', width='150')
+
+def uzvaras_gui():
+    global uzvaras_teksts
+    root.unbind("<Key>")
+    ekrans.delete(minējuma_burts,minējuma_teksts)
+    uzvaras_teksts = ekrans.create_text(80, 150, anchor='w', text="Tu uzvarēji!", fill="black", font=("Helvetica", 30))
+    poga_spēlēt_atkal.place(x=120, y=460, anchor="center", height='30', width='150')
+    dzēst_minēšanas_vārda_gui()
 
 '''
 =======Sākuma ekrāns, pogas========
 '''
 nosaukums = ekrans.create_text(x/2, 150, anchor="center", text="Karātavas", fill="black", font=("Helvetica", 30))
-poga = Button(ekrans, text='Spēlēt',command=jauna_spēle)
-pogaZaude = Button(ekrans,text='Spēlēt atkal?',command=pirms_jauna_spēle)
-poga.place(x=x/2,y=y/2, anchor="center", height='30',width='150')
+mošķa_teksts = ekrans.create_text(x/2, 300, anchor="center", text="Izvēlies mošķi", fill="black", font=("Helvetica", 15))
+mošķu_izvēle = OptionMenu(root, chosen_char, *Chars.values())
+mošķu_izvēle.place(x=x/2, y=350, anchor="center", height='25', width='150')
+poga = Button(root, text='Spēlēt', command=jauna_spēle)
+poga_spēlēt_atkal = Button(root, text='Spēlēt atkal?', command=pirms_jauna_spēle)
+poga.place(x=x/2, y=220, anchor="center", height='30', width='150')
 
 '''
 =======Zīmējuma daļu rādīšana========
 '''
 
 class KarātavasDaļas:
-    def __init__(self,detaļas):
+    def __init__(self, detaļas):
         self.detaļas = list(detaļas)
         self.skaits = 0
 
     def parādīt_detaļu(self):
-        command_name = self.detaļas[self.skaits][0][0]
-        command_args = self.detaļas[self.skaits][1]
-        command = getattr(ekrans, command_name)
-        var_name = f'var_{self.skaits}'
-        globals()[var_name] = command(*command_args)
-        self.skaits += 1
-    
+        if self.skaits < len(self.detaļas):
+            command_name = self.detaļas[self.skaits][0][0]
+            command_args = self.detaļas[self.skaits][1]
+            command = getattr(ekrans, command_name)
+            var_name = f'var_{self.skaits}'
+            globals()[var_name] = command(*command_args)
+            self.skaits += 1
+
     def dzēst_parādītās_detaļas(self):
         for i in range(self.skaits):
             var_name = f'var_{i}'
             if var_name in globals():
                 ekrans.delete(globals()[var_name])
         self.skaits = 0
-        
 
 Char = KarātavasDaļas(Spoks)
-
 
 '''
 =======Spēles loģiskā daļa========
@@ -175,30 +201,26 @@ def pogas_spiediens(event):
 def minēt():
     global char, vārds, kļūdas, minētie_burti
     if char in vārds:
-        print("cepums tev")
-        vārds = izdzēst_char(vārds,char)
+        
+        vārds = izdzēst_char(vārds, char)
         minētie_burti += char
-        print(vārds)
+        #print(vārds)
         minēšanas_burta_gui()
         if vārds == '':
-            print("woow, malacītis!")
+            uzvaras_gui()
     elif char in minētie_burti:
-        print("dunduk,",char,"burts jau minēts")
+        pass #not used
     else:
-        minētie_burti+=char
-        kļūdas+=1
+        minētie_burti += char
+        kļūdas += 1
         Char.parādīt_detaļu()
-        print("Kļūdu skaits:",kļūdas,char)
+        #print("Kļūdu skaits:", kļūdas, char)
         if kļūdas == pieļaujamais_kļūdu_skaits:
-            print("losis")
-            ekrans.after(1000)
             zaudēšanas_gui()
-            pass
+            ekrans.after(1000)
 
 def izdzēst_char(word, char):
     return word.replace(char, '')
 
-
-root.bind("<Key>", pogas_spiediens)
 
 root.mainloop()
